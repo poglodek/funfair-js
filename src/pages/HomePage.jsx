ffd7c9e2318e24096762c26bcb812c91107e544f
poglodek <pawelogll@gmail.com>
2023-07-03 21:33:10 +0200

    adding navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,15 @@
 
 export function Home() {
-    return (        
-        <h1>Home Page!</h1>
+    return (   
+        <>
+            <div className="flex justify-center text-5xl drop-shadow-2xl  p-5 m-4">
+                 <p>Welcome to FunfAir airlines</p>
+             </div>
+             <div>
+                <p className=" m-4 p-5 bg-gray-800 bg-opacity-30 rounded-3xl drop-shadow-2xl text-l">
+                Funfair Airlines: The epitome of joy in the sky. Experience the thrill of flying with our exciting destinations, exceptional service, and vibrant atmosphere. Let us take you on an unforgettable journey filled with laughter, adventure, and pure fun. Buckle up for a joyride like no other!
+                </p>
+             </div>
+        </>
      );
   }
