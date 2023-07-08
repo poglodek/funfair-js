f48c38dc3ff37ca6bf20a905d2fcde3edcbfa366
poglodek <pawelogll@gmail.com>
2023-07-08 21:01:09 +0200

    adding navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import { Home } from './pages/HomePage';
 import { Login } from './pages/LoginPage';
 import { NavBar } from './pages/NavBar';
@@ -8,15 +8,13 @@ import { NavBar } from './pages/NavBar';
 function App() {
   return (
       <div className="bg-gradient-to-t from-gray-900 to-gray-700 text-white p-4 h-screen">
-         <NavBar />
-        <BrowserRouter>
-       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-     </BrowserRouter>
+        <NavBar />
+        <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="/login" element={ <Login/> } />
+      </Routes>
       </div>
-   );
+  );
 }
 
 export default App;
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,11 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
 
 export function NavBar() {
-    return (        
-        <>
-            <div className="bg-blue-900 bg-opacity-25 w-auto h-14 text-white flex rounded-xl mr-3 ml-3 p-3 pb-3 space-x-3 justify-end">
-                <button className="rounded-md  p-2 justify-center flex items-center transition-all duration-300 hover:bg-blue-600"> Home</button>
-                <button className="rounded-md  p-2 justify-center flex items-center transition-all duration-300 hover:bg-blue-600 mr-0 "> Login</button>
-            </div>
-        </>
-     );
-  }
+  return (
+      <div className="bg-blue-900 bg-opacity-25 w-auto h-14 text-white flex rounded-xl mr-3 ml-3 p-3 pb-3 space-x-3 justify-end">
+        <Link to="/" className="rounded-md p-2 justify-center flex items-center transition-all duration-300 hover:bg-blue-600">Home</Link>
+        <Link to="/login" className="rounded-md p-2 justify-center flex items-center transition-all duration-300 hover:bg-blue-600 mr-0">Login</Link>
+      </div>
+  );
+}
