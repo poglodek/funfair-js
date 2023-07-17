60b561e7f5cbc05909c347ea61bda6de8a0de11e
poglodek <pawelogll@gmail.com>
2023-07-17 18:31:32 +0200

    adding popups

---
+++ src/components/ScreenPopup.jsx
@@ -0,0 +1,24 @@
+
+import React, { useState } from 'react';
+import ReactModal from 'react-modal';
+import { Button } from './Button';
+
+
+export default function ScreenPopup({ text, isOpen, setIsOpen }) {
+
+   
+    return (
+      <div >
+        <ReactModal
+        overlayClassName="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+        className="bg-white p-6 rounded-md shadow-md"
+          isOpen={isOpen}
+          contentLabel="Popup!"
+          onRequestClose={() => setIsOpen(false)} >
+          {text} <br /><br />
+          <Button text="OK"  callBack={()=> setIsOpen(false)}/>
+        </ReactModal>
+      </div>
+    );
+  }
+  
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,13 +3,16 @@ import { Submit } from '../components/Submit';
 import { LinkButton } from '../components/LinkButton';
 import { Input } from '../components/Input';
 import { useNavigate } from 'react-router-dom';
+import ScreenPopup from '../components/ScreenPopup';
 
 
 
 export function Login({ setUser }) {
     const [mail, setMail] = useState('');
     const [password, setPassword] = useState('');
+    const [isOpen, setIsOpen] = useState(false);
     const navigate = useNavigate();
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -22,11 +25,14 @@ export function Login({ setUser }) {
             'token': true
         });
 
-        navigate('/');
+        setIsOpen(true);
+      //  navigate('/');
     };
+
 
     return (
         <div className="justify-center flex">
+        <ScreenPopup text="Login successfully!" isOpen={isOpen} setIsOpen={setIsOpen}  />
             <div className="max-w-md mx-auto mt-8">
                 <form onSubmit={handleSubmit} className="bg-gray-600 shadow-md rounded px-8 pt-6 pb-8 mb-4">
                     <Input id="mail" type="mail" placeholder="Mail" value={mail} setValue={setMail} />
